fc7eb32707eb65a72d44ef3e934c50e95f5aadd6
Roman Serebriakov <rserebryakov1902@gmail.com>
2023-01-16 19:24:19 +0200

    postinstall fix

---
+++ util/postinstall.js
@@ -0,0 +1,19 @@
+var package = require("../fixture/package.json");
+let dependencies = [];
+for (let key in package.dependencies) {
+  dependencies.push(`${key}@${package.dependencies[key]}`)
+}
+
+var npm = require("npm");
+npm.load({
+  loaded: false
+}, function (err) {
+  // catch errors
+  npm.commands.install(dependencies, function (err, data) {
+    // log the error or data
+  });
+  npm.on("log", function (message) {
+    // log the progress of the installation
+    console.log(message);
+  });
+});
