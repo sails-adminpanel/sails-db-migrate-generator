95c8e491078038ed6ce28dc6a50b9e4d5b3b3bf7
Xziy <x@42.pub>
2023-08-23 18:17:59 +0300

    change postinstall script

--- util/postinstall.js
+++ util/postinstall.js
@@ -1,43 +1,12 @@
-const path = require("path");
-const { applyPatchesForApp } = require("patch-package");
-const { readFileSync } = require('fs');
-const packageJsonPath = path.resolve(__dirname, "../fixture/package.json");
-const packageJson = JSON.parse(readFileSync(packageJsonPath, 'utf8'));
-process.chdir(path.resolve(__dirname, "../fixture"));
-const dependencies = Object.keys(packageJson.dependencies).map(key => `${key}@${packageJson.dependencies[key]}`);
+const { execSync } = require('child_process');
+const path = require('path');
 
-const npm = require("npm");
-npm.load({
-  loaded: false
-}, function (err) {
-  if (err) {
-    console.error(err);
-    return;
-  }
+const appPath = path.resolve(__dirname, "../fixture");
 
-  // Перенесено наверх, после npm.load
-  npm.commands.install(dependencies, function (err, data) {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    
-    const appPath = path.resolve(__dirname, "../fixture");
-    const reverse = false;
-    const patchDir = "node_modules/dark-sails/";
-    const shouldExitWithError = false;
-    const shouldExitWithWarning = false;
-
-    applyPatchesForApp({
-      appPath,
-      reverse,
-      patchDir,
-      shouldExitWithError,
-      shouldExitWithWarning
-    });
-  });
-
-  npm.on("log", function (message) {
-    console.log(message);
-  });
-});
+try {
+  process.chdir(appPath);
+  execSync('npm install', { stdio: 'inherit' });
+  execSync('npm run postinstall', { stdio: 'inherit' });
+} catch (error) {
+  console.error ('Error:', error.message);
+}
